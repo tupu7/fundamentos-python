7a235fe3d29696ecdd389617fcaaf75c6d493b19
Eduardo Figueroa <edunoe3006@gmail.com>
2025-05-21 02:11:47 -0600

    Ho

---
+++ practica1.py
@@ -0,0 +1,61 @@
+# a = int(input("Total de la cuenta: "))
+# b = int(input("Propina: "))
+# c = (f"Total de la cuenta con propina {b/a * 100}%: {a + b}$" )
+# print((c))
+
+
+# a = (input("Escriba su primer nombre: "))
+# b = (input("Escriba su segundo nombre: "))
+# c = (input("Escriba su primer apellido: "))
+# d = (input("Escriba su segundo apellido: "))
+# print(f"El correo que se debe asignar al usuario ingresado es:\n{a + "." + c + "@keyinstitute.edu.sv"}")
+
+
+# a = int(input("Primer numero: "))
+# b = int(input("Segundo numero: "))
+# k = int(input("Numero de decimales: "))
+a=5
+b=3
+
+c = a/b
+c = str(c)
+c = list(c)
+k = 2
+
+print(c[k:])
+
+
+
+# a = input("Coloca la contraseña: ")
+
+
+# mayus = False
+# digito = False
+
+# if len(a) >= 8:
+#     for caracter in a:
+#         if caracter.isupper():
+#             mayus = True
+#         if caracter.isdigit():
+#             digito = True
+#     if mayus and digito:
+#         print("Contraseña segura") 
+#     else:
+#         print("Constraseña no segura")
+
+        
+
+
+
+# a = (int(input("Ponga el valor de las unidades: ")))
+
+# if 0 <= a <= 100:
+#     print("0")
+
+# elif 101  <= a <= 200:
+#     print((a * 0.5)) 
+
+# elif a >= 201:
+#     print(a * 0.7)
+
+
