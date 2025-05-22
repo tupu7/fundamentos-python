185d49dbabb5c64131f6342277b283c78bd58914
Eduardo Figueroa <edunoe3006@gmail.com>
2025-05-22 06:43:36 -0600

    Dennys

--- practica1.py
+++ practica1.py
@@ -1,8 +1,33 @@
+'''
+Clase:        Correlativo de clase
+Tema:         Variables, tipos, entradas y salidas
+Ejercicio:    1.3.1. Automatizando el cálculo de la propina
+Descripción:  Pide al usuario el total de una cuenta y el porcentaje de propina
+(por ejemplo, 10%, 15%, 20%). Calcula y muestra en pantalla el total
+a pagar.
+
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
 # a = int(input("Total de la cuenta: "))
 # b = int(input("Propina: "))
 # c = (f"Total de la cuenta con propina {b/a * 100}%: {a + b}$" )
 # print((c))
 
+'''
+Clase:        Correlativo de clase
+Tema:         Variables, tipos, entradas y salidas
+Ejercicio:    1.3.2. Generador del correo de Key
+Descripción:  Solicita al usuario su nombre completo (asume dos nombres y
+dos apellidos). Luego, el programa generará, su correo con el
+formato:
+• primer_nombre.primer_apellido@keyinstitute.edu.sv
+
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
 
 # a = (input("Escriba su primer nombre: "))
 # b = (input("Escriba su segundo nombre: "))
@@ -10,21 +35,47 @@
 # d = (input("Escriba su segundo apellido: "))
 # print(f"El correo que se debe asignar al usuario ingresado es:\n{a + "." + c + "@keyinstitute.edu.sv"}")
 
+'''
+Clase:        Correlativo de clase
+Tema:         Variables, tipos, entradas y salidas
+Ejercicio:    1.4.1. División con Precisión
+Descripción:  Dados tres enteros a, b y k, imprime el resultado de a / b con k decimales exactos (sin
+redondear)
+
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
 
 # a = int(input("Primer numero: "))
 # b = int(input("Segundo numero: "))
-# k = int(input("Numero de decimales: "))
-a=5
-b=3
+# k = int(input("Decimales: "))
 
-c = a/b
-c = str(c)
-c = list(c)
-k = 2
+# numero = a/b
+# numero = str(numero).split(".")
+# deci = len(numero[1])
+# entero = len(numero[0])
 
-print(c[k:])
+# if deci == k:
+#     print(numero[0] + "." +  numero[1])
+# elif deci < k:
+#     falta = k - deci
+#     print(numero[0] + "." + 0*falta)
+# else:
+#     print(numero[0] + "." + numero[1][:k])
 
+'''
+Clase:        Correlativo de clase
+Tema:         Bloque condicional
+Ejercicio:    2.3.1. Contraseña segura
+Descripción:  Solicita una cadena de texto que representa una contraseña, y verifica si la contraseña
+cumple con las siguientes condiciones: tener al menos 8 caracteres, un número y una
+mayúscula.
 
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
 
 # a = input("Coloca la contraseña: ")
 
@@ -39,13 +90,25 @@ print(c[k:])
 #         if caracter.isdigit():
 #             digito = True
 #     if mayus and digito:
-#         print("Contraseña segura") 
+#         print("Contraseña segura")
 #     else:
 #         print("Constraseña no segura")
 
-        
 
 
+
+'''
+Clase:        Correlativo de clase
+Tema:         Bloque condicional
+Ejercicio:    2.3.2. Cálculo de impuesto
+Descripción:  Desarrollar un programa en Python que permita calcular el impuesto que debe pagar
+un usuario por el consumo de energía. El cálculo debe realizarse basado en la siguiente
+tabla.
+
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
 
 # a = (int(input("Ponga el valor de las unidades: ")))
 
@@ -53,9 +116,128 @@ print(c[k:])
 #     print("0")
 
 # elif 101  <= a <= 200:
-#     print((a * 0.5)) 
+#     print((a * 0.5))
 
 # elif a >= 201:
 #     print(a * 0.7)
 
+
+
+
+
+
+
+#Suma de cadenas
+
+# a = "12345678901234567890"
+# b = "98765432109876543210"
+
+# a = int(a[-1])
+# print(a)
+
+# b = int(b[-1])
+# print(b)
+
+#Fracciones irreducibles
+
+
+# n = int(input("Fracciones: "))
+# if n > 0:
+#     x = int(input("Numerador: "))
+#     y = int(input("denominador: ")) 
+#     print(x / y)
+#     x = str(x)
+#     y = str(y)
+#     print(x + "/" + y)
+
+
+'''
+Clase:        Correlativo de clase
+Tema:         Bloque condicional
+Ejercicio:    2.4.1. El número mágico
+Descripción:  Crea un programa en Python para determinar si un número es "mágico“.
+Un número es “mágico” si es divisible por 7 pero no por 5.
+
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
+
+#Numero magico
+# a = 35
+
+# magico = False
+# normal = False
+
+# if a % 7 == 0 and a % 5 != 0: 
+#     magico = True
+# else:
+#     normal = True
+
+# if magico and not normal:
+#     print("magico")
+# else:
+#     print("normal")
+
+#Año Bisiesto
+'''
+Clase:        Correlativo de clase
+Tema:         Bloque condicional
+Ejercicio:    2.4.2. Año bisiesto
+Descripción:  Determina si un año es bisiesto, considerando las reglas gregorianas. Para que un año
+sea bisiesto, debe cumplir alguna de las siguientes condiciones:
+• Si es divisible por 4.
+• Si es divisible por 100 y por 400.
+
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
+
+# a = 1900
+
+# if a % 4 ==0 or (a % 100 == 0 and a % 400 == 0):
+#     print("año bi")
+# else:
+#     print("Año normal")
+
+#Elevador
+'''
+Clase:        Correlativo de clase
+Tema:         Bloque condicional
+Ejercicio:    2.5.1. Elevador lógico
+Descripción:  En un edificio hay dos elevadores (A y B) en distintos pisos. Un usuario llama al elevador
+desde un piso p. El elevador más cercano responde, pero si ambos están a la misma
+distancia, elige el A.
+
+Autor:        Eduardo Noé Figueroa Rodríguez
+Fecha:        2025-05-15
+Estado:       [ Terminado ]
+'''
+
+# import math
+
+# a = 10
+# b = 7
+
+# p = 9
+
+# elevaddor_A = math.fabs(a-p)
+# elevaddor_B = math.fabs(b-p)
+
+# if elevaddor_A < elevaddor_B:
+#     print("Elevador A")
+# elif elevaddor_A > elevaddor_B:
+#     print("Elevador B")
+# else:
+#     print("Elevador A")
+
+#Zona de peligro
+# x = 5
+# y = -10
+
+# if x**2 + y**2 <= 25:
+#     print("Peligro")
+# else:
+#     print("Seguro")
 
