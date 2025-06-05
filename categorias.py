16f82aa005bd9ca3a81f32cb37698efe42b42665
Eduardo Figueroa <edunoe3006@gmail.com>
2025-06-04 19:26:07 -0600

    Dennys

--- categorias.py
+++ categorias.py
@@ -1,7 +1,47 @@
-Categorias = {
-    "Cosas importantes":{"Noticias": "noticias",
-                        "Eventos": "eventos",
-                        "Anuncios": "anuncios",
-                        "Alertas": "alertas"},
+categorias = {
+    "Torneos de deportes": {
+        "Fútbol": {"Torneo de fútbol principiantes": "futbol_principiantes",
+                    "Torneo de fútbol intermedio": "futbol_intermedio",
+                    "Torneo de fútbol avanzado": "futbol_avanzado",
+        },
+        "Baloncesto": {"Torneo de baloncesto principiantes": "baloncesto_principiantes",
+                       "Torneo de baloncesto intermedio": "baloncesto_intermedio",
+                       "Torneo de baloncesto avanzado": "baloncesto_avanzado",
+        },
+        "Ping pong":{"Torneo de ping pong principiantes": "ping_pong_principiantes",
+                     "Torneo de ping pong intermedio": "ping_pong_intermedio",
+                    "Torneo de ping pong avanzado": "ping_pong_avanzado",
+        },
+    },
+
+    "Menú de los almuerzos del día": {
+        "Lunes": "El menú del lunes",
+        "Martes": "El menú del martes",
+        "Miércoles": "El menú del miércoles",
+        "Jueves": "El menú del jueves",
+        "Viernes": "El menú del viernes",
+    },
+
+    "Actividades adicionales": {
+        "Salidas culturales": "Actividades como visitas a museos o excursiones culturales",
+        "Eventos especiales": "Actividades como cuando vinieron los de la NASA o lo del IAAC ",
+        "Tallerres": "Wellness Key o De procrastinación",
+    },
+
+    "Actividades obligatorias": {
+        "KeyWeek": { 
+            "Actividades del lunes": {"Descripción": "Descripción de las actividades del lunes", "horario": "9:00 AM - 5:00 PM"},
+            "Actividades del martes": {"Descripción": "Descripción de las actividades del martes", "horario": "9:00 AM - 5:00 PM"},
+            "Actividades del miércoles": {"Descripción": "Descripción de las actividades del miércoles", "horario": "9:00 AM - 5:00 PM"},
+        },
+        "HorasKey": {
+            "Activiad uno": {"Descripción": "Descripción de la actividad uno", "Cuantas horas": "1 hora"},
+            "Actividad dos": {"Descripción": "Descripción de la actividad dos", "Cuantas horas": "2 horas"},
+            "Actividad tres": {"Descripción": "Descripción de la actividad tres", "Cuantas horas": "3 horas"},
+
+        },
 }
-print("Categorias:", Categorias)
+}
+
+
+print("Categorías disponibles:", list(categorias.keys()))
